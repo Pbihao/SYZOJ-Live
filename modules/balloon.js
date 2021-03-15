bf85b53a976ca7a7f05b2209d4c61f7f93e390ed
Pbihao <1435343052@qq.com>
2021-03-15 22:38:09 +0800

    update balloons

--- modules/balloon.js
+++ modules/balloon.js
@@ -81,6 +81,88 @@ app.get('/balloons', async (req, res) => {
         result: getRoughResult(x, displayConfig, true),
         running: false,
       })),
+      in_read: false,
+      contestId: contestId,
+      paginate: paginate,
+      pushType: 'rough',
+      form: req.query,
+      displayConfig: displayConfig,
+      fast_pagination: syzoj.config.submissions_page_fast_pagination
+    });
+  } catch (e) {
+    syzoj.log(e);
+    res.render('error', {
+      err: e
+    });
+  }
+});
+
+
+
+app.get('/balloons/read', async (req, res) => {
+  try {
+
+    const curUser = res.locals.user;
+
+
+    const contestId = Number(req.query.contest);
+    const contest = await Contest.findById(contestId);
+    contest.ended = contest.isEnded();
+    if (!(curUser && await contest.isWatcher(curUser))) {
+      throw new Error("您暂时无权查看此比赛的气球榜单信息。");
+    }
+
+
+    let query = JudgeState.createQueryBuilder("mt");
+
+    query.andWhere('type = 1');
+    query.andWhere('type_info = :type_info', { type_info: contestId });
+    query.andWhere('balloon_checked=:checked', {checked:1});
+
+    query.innerJoin(
+      qb =>
+        qb.from(JudgeState, 't')
+          .select('MIN(submit_time)', 'MinDate')
+          .addSelect("user_id")
+          .addSelect("problem_id")
+          .andWhere("type=1")
+          .andWhere('type_info=:type_info', {type_info: contestId})
+          .andWhere('status="Accepted"')
+          .groupBy("user_id, problem_id")
+      ,
+      "t",
+      'mt.submit_time=t.MinDate AND mt.user_id=t.user_id AND mt.problem_id=t.problem_id'
+    );
+
+    let judge_state, paginate;
+
+    paginate = syzoj.utils.paginate(
+      await JudgeState.countQuery(query),
+      req.query.page,
+      syzoj.config.page.judge_state
+    );
+    judge_state = await JudgeState.queryPage(paginate, query, { id: "ASC" }, true);
+
+
+    await judge_state.forEachAsync(async obj => {
+      await obj.loadRelationships();
+    });
+
+    //console.log(req.query.status)
+
+    res.render('balloons', {
+      items: judge_state.map(x => ({
+        info: getSubmissionInfo(x, displayConfig),
+        token: (x.pending && x.task_id != null) ? jwt.sign({
+          taskId: x.task_id,
+          type: 'rough',
+          displayConfig: displayConfig
+        }, syzoj.config.session_secret) : null,
+        result: getRoughResult(x, displayConfig, true),
+        running: false,
+      })),
+      in_read: true,
+      contestId: contestId,
       paginate: paginate,
       pushType: 'rough',
       form: req.query,
